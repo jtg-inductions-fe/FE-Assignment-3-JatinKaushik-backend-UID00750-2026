baf166c65cafa947ce96d937e74d4c9ea5724a8e
Jatin Kaushik <jatin@joshtechnologygroup.com>
2026-09-05 12:50:14 +0530

    [JK_A3_05]: Update menu module with correct imports

--- src/modules/menu/menu.module.ts
+++ src/modules/menu/menu.module.ts
@@ -3,12 +3,12 @@ import { MenuService } from './menu.service';
 import { MenuController } from './menu.controller';
 import { MenuItemRepository } from './repositories/menu-item.repository';
 import { MenuCategoryRepository } from './repositories/menu-category.repository';
-import { RestaurantRepository } from '@modules/restaurants/repositories/restaurants.repository';
+import { RestaurantsModule } from '@modules/restaurants/restaurants.module';
 
 @Module({
-    imports: [RestaurantRepository],
+    imports: [RestaurantsModule],
     controllers: [MenuController],
     providers: [MenuService, MenuItemRepository, MenuCategoryRepository],
-    exports: [MenuService, MenuCategoryRepository],
+    exports: [MenuService, MenuCategoryRepository, MenuItemRepository],
 })
 export class MenuModule {}
